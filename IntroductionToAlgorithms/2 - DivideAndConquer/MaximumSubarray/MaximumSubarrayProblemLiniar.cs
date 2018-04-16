e7c2fc5a23246a6d7a0e6e55e2c3ab9be0fc88c7
Sergey <sergeivrusskikh@gmail.com>
2018-04-16 22:46:58 +0300

    fastre subarray problem solution

---
+++ IntroductionToAlgorithms/2 - DivideAndConquer/MaximumSubarray/MaximumSubarrayProblemLiniar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DivideAndConquer.MaximumSubarray
+{
+    /// <summary>
+    /// Limple solution for finding max subarray problem.
+    /// 
+    /// Complexity - O(n)
+    /// </summary>
+    public class MaximumSubarrayProblemLiniar
+    {
+        public int[] Run(int[] input)
+        {
+            var resultSum = input[0];
+	        var left = 0;
+	        var right = 0;
+	        var sum = 0;
+	        var minusPos = -1;
+
+            for (int i = 0; i < input.Length; i++) 
+            {
+	            sum += input[i];
+ 
+	            if (sum > resultSum) {
+		            resultSum = sum;
+		            left = minusPos + 1;
+		            right = i;
+	            }
+ 
+	            if (sum < 0) {
+		            sum = 0;
+		            minusPos = i;
+	            }
+            }
+
+            var result = new int[right - left + 1];
+            Array.Copy(input, left + 1, result, 0, right - left + 1);
+
+            return result;
+        }
+    }
+}
--- IntroductionToAlgorithms/2 - DivideAndConquer/MaximumSubarray/MaximumSubarrayProblemTests.cs
+++ IntroductionToAlgorithms/2 - DivideAndConquer/MaximumSubarray/MaximumSubarrayProblemTests.cs
@@ -12,9 +12,17 @@ namespace DivideAndConquer.MaximumSubarray
         }
 
         [Fact]
-        public void RunSorting_WithExampleInput_ReturnsSortedSequence()
+        public void RunMaxSliceFinding_WithExampleInput_ReturnsMaxSlice()
         {
             var input = new[] { 100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97 };
+            var result = _maximumSubarray.Run(input);
+            Assert.Equal(new[] { 81, 101, 94, 106 }, result);
+        }
+
+        [Fact]
+        public void RunMaxSliceFinding_WithNegativeInput_ReturnsMaxSlice()
+        {
+            var input = new[] { 10, 9, 8, 7 };
             var result = _maximumSubarray.Run(input);
             Assert.Equal(new[] { 81, 101, 94, 106 }, result);
         }
