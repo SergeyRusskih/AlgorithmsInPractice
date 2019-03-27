51799c71ce02c5b9bf312b6d40fa9fc099a53973
Sergey <sergey_russkih89@mail.ru>
2019-03-27 14:16:26 +0300

    cutting strings

---
+++ DynamicProgramming/TopDown/CuttingSticks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DynamicProgramming.TopDown
+{
+    public class CuttingSticks
+    {
+        private int FindMinimalCutCost(int stick)
+        {
+            if (stick == 0)
+                return 0;
+            return -1;
+        }
+
+        [Fact]
+        public void Should_Find_Minimal_Cost()
+        {
+            var stick = 100;
+
+        }
+    }
+}
