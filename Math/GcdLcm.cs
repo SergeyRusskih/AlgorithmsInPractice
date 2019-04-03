9b3ce694a6a9f8bf2ac82d61067624768dfd1790
Sergey <sergey_russkih89@mail.ru>
2019-04-03 16:59:47 +0300

    gcd and llcm

---
+++ Math/GcdLcm.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Math
+{
+    public class GcdLcm
+    {
+        private int FindGcd(int first, int second)
+        {
+            if (second == 0)
+                return first;
+
+            return FindGcd(second, first % second);
+        }
+
+        private int FindLcm(int first, int second)
+        {
+            return first * (second / FindGcd(first, second));
+        }
+
+        [Theory]
+        [InlineData(4, 8, 4)]
+        [InlineData(10, 5, 5)]
+        [InlineData(20, 12, 4)]
+        public void Should_Have_Gcd(int first, int second, int expected)
+        {
+            var result = FindGcd(first, second);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4, 8, 8)]
+        [InlineData(10, 5, 10)]
+        [InlineData(20, 12, 60)]
+        public void Should_Have_Lcm(int first, int second, int expected)
+        {
+            var result = FindLcm(first, second);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Math/RelativelyPrime.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Math
+{
+    /// <summary>
+    /// Euler’s Totient (Phi) function
+    /// </summary>
+    public class RelativelyPrime
+    {
+        [Theory]
+        [InlineData(25, 42)]
+        [InlineData(25, 42)]
+        public void Should_Be_Prime(int a, int b)
+        {
+
+        }
+    }
+}
